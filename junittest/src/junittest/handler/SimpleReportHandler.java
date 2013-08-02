850aa1005707f92304d709f1d83ba5a3d7984725
Michael <Michael@Michael-PC>
2013-08-02 19:17:05 +0800

    1

--- junittest/src/junittest/handler/SimpleReportHandler.java
+++ junittest/src/junittest/handler/SimpleReportHandler.java
@@ -14,13 +14,17 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -36,26 +40,50 @@ public class SimpleReportHandler extends AbstractHandler implements IHandler {
 		
 		IStructuredSelection selection = (IStructuredSelection) view.getTableViewer().getSelection();
 		if(!selection.isEmpty()){
-			IResource res = (IResource) selection.getFirstElement();
-			String jasperfile = "reports/report4.jasper";
-			String filetype = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.REPORT_FILE_TYPE);
+			final IResource res = (IResource) selection.getFirstElement();
+			final String jasperfile = "reports/report4.jasper";
+			final String filetype = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.REPORT_FILE_TYPE);
 //			String destFile = ResourceManager.getInstance().getProject().getFolder(ResourceManager.FOLDER_REPORT).getLocation().append(FILE_NAME).addFileExtension(filetype.toLowerCase()).toOSString();
 			FileDialog dialog = new FileDialog(window.getShell(), SWT.SAVE);
 			dialog.setFilterExtensions(new String[]{"*." + filetype.toLowerCase()});
 			dialog.setFileName(FILE_NAME);
 			dialog.setOverwrite(true);
-			String destFile = dialog.open();
+			final String destFile = dialog.open();
 			if(destFile == null) return null;
-			try {
-//				InputStream is = Activator.getDefault().getBundle().getEntry(jasperfile).openStream();
-				ReportUtils.generateReport(res, FileLocator.resolve(Activator.getDefault().getBundle().getEntry(jasperfile)).getFile(), filetype, destFile);
-//				is.close();
-				MessageDialog.openInformation(window.getShell(), "提示", "报告生成成功，路径为" + destFile);
-			} catch (JRException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				ErrorDialog.openError(window.getShell(), "错误", "生成报告失败", new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getLocalizedMessage(),e));
-			}
+			Job job = new Job("生成报告") {
+				
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					// TODO Auto-generated method stub
+					monitor.beginTask("生成报告", IProgressMonitor.UNKNOWN);
+					try {
+//						InputStream is = Activator.getDefault().getBundle().getEntry(jasperfile).openStream();
+						ReportUtils.generateReport(res, FileLocator.resolve(Activator.getDefault().getBundle().getEntry(jasperfile)).getFile(), filetype, destFile);
+//						is.close();
+						Display.getDefault().asyncExec(new Runnable() {
+							
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								MessageDialog.openInformation(new Shell(), "提示", "报告生成成功，路径为" + destFile);
+							}
+						});
+					} catch (JRException | IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						Display.getDefault().asyncExec(new Runnable() {
+							
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								ErrorDialog.openError(new Shell(), "错误", "生成报告失败", new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getLocalizedMessage(),e));
+							}
+						});
+					}
+					monitor.done();
+					return Status.OK_STATUS;
+				}
+			};
 		}
 		return null;
 	}
