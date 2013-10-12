f03d32fa51ad75d28c0da1702e813a693c08fcc9
Michael <Michael@Michael-PC>
2013-10-12 23:02:23 +0800

    1

--- junittest/src/junittest/view/LogView.java
+++ junittest/src/junittest/view/LogView.java
@@ -32,7 +32,6 @@ import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.part.ViewPart;
 
